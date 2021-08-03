59f1d1fef34b44dd6b447ca93b198647af049ca8
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-08-04 04:07:48 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,7 +10,7 @@ Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
 Jadi Jangan takut ditipu,santayy aja kali🐈
 """
     BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
-    ERRED_PAGE = "Hadeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @mondarajedah"
+    ERRED_PAGE = "Hadeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @monajedah"
     CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang"
     IN_VALID_CODE_PVDED = "Baca Yang Benar Goblok😈Masukan Kode Telegram yang Bener 😁🙏"
     IN_VALID_PHNO_PVDED = "Lu Buta? Masukan NO HP yang Bener Goblok 😁🙏 Pake Format +(kode Negara) Contoh +62(nohp)"
